9c04cd4620b2b8a9b1b1cc498ba7248f27599202
FrancesCC <holyspirit1515@gmail.com>
2020-09-18 20:43:27 -0400

    going to create branches.

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.androidexamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main_grid);
         setContentView(R.layout.activity_main);
+
+//        ImageButton iBtn = findViewById(R.id.image_Button);
+//        iBtn.setOnClickListener( btn -> {} );
+
+//        CheckBox cb = (CheckBox) findViewById(R.id.checkBtn);
+//        cb.setChecked(true);
+
+
+ //       Switch sw = findViewById(R.id.switchBtn);
+
+
     }
 }
