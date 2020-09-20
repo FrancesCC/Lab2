a2f2eab79c2f84d0f9c4368e8d1dee92c6f60205
FrancesCC <holyspirit1515@gmail.com>
2020-09-19 22:19:31 -0400

    finished version

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -1,28 +1,59 @@
 package com.example.androidexamples;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+
+//implements CompoundButton.OnCheckedChangeListener
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main_grid);
-        //setContentView(R.layout.activity_main_linear);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_relative);
 
 
-        CheckBox cb = (CheckBox) findViewById(R.id.checkBtn);
+
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(MainActivity.this,getResources().getString(R.string.toast_message),Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        CheckBox cb = findViewById(R.id.checkBtn);
         cb.setChecked(true);
 
-
-        Switch sw = findViewById(R.id.switchBtn);
-
-
+        Switch mySwitch =  findViewById(R.id.switchBtn);
+        mySwitch.setOnCheckedChangeListener(this);
     }
+
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        View v = findViewById(R.id.root_view);
+
+        String messege = null;
+        if(b){
+            messege = getResources().getString(R.string.switchoff);
+        }
+        else
+            messege = getResources().getString(R.string.snackMessage);
+
+        Snackbar snack = Snackbar.make(v,messege,Snackbar.LENGTH_SHORT);
+        snack.setAction(getResources().getString(R.string.undo), click -> compoundButton.setChecked(!b));
+        snack.show();
+    }
+
 }
